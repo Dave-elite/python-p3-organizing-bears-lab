bbecb68ef001627605f1a2dba236937b78914563
David Waruhiu Njenga <davenjenga098@gmail.com>
2024-09-08 21:29:28 +0300

    lab complete

---
+++ lib/create.sql
@@ -0,0 +1,10 @@
+CREATE TABLE bears(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name VARCHAR(255),
+    age INTEGER(230),
+    sex VARCHAR(255),
+    color VARCHAR(255),
+    temperament VARCHAR(255),
+    alive BOOLEAN
+
+);
---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES 
+('Mr.Chocolate', 8, 'M', 'Black', 'Friendly', 1),
+('Rowdy', 8, 'M', 'Black', 'Aggressive', 0),
+('Tabitha', 5, 'M', 'White', 'Calm', 1),
+('Sergeant', 10, 'F', 'Brown','Stern', 1),
+('Melissa', 11, 'F', 'Red', 'Grumpy', 0),
+('Grinch', 12, 'F','Orange', 'Playful', 1),
+('Wendy', 13, 'F', 'Black', 'Neutral', 1),
+(NULL, 0, 'F', 'Black', 'Queit', 0)
